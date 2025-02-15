cafa99fa577392ff70e4b5d5e7bfe4ed704196e9
haicoool <haicoool@users.noreply.github.com>
2025-02-16 05:21:22 +0800

    Update opportunities.blade.php

--- resources/views/organization/opportunities.blade.php
+++ resources/views/organization/opportunities.blade.php
@@ -15,26 +15,17 @@
                     <th class="py-3 px-4 text-left text-gray-600 font-semibold">Title</th>
                     <th class="py-3 px-4 text-left text-gray-600 font-semibold">Description</th>
                     <th class="py-3 px-4 text-left text-gray-600 font-semibold">Location</th>
-                    <th class="py-3 px-4 text-center text-gray-600 font-semibold">Actions</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($opportunities as $opportunity)
-                    <tr class="border-b hover:bg-gray-100">
+                    <tr
+                        class="border-b hover:bg-gray-100 cursor-pointer"
+                        onclick="window.location='{{ $opportunity->oppDate->toDateString() === $currentDate->toDateString() ? route('registrations.show', $opportunity->oppId) : 'javascript:void(0)' }}'"
+                    >
                         <td class="py-4 px-4 text-left">{{ $opportunity->oppTitle }}</td>
                         <td class="py-4 px-4 text-left">{{ $opportunity->oppDesc }}</td>
                         <td class="py-4 px-4 text-left">{{ $opportunity->oppLocation }}</td>
-                        <td class="py-4 px-4 text-center">
-                            @if($opportunity->oppDate->toDateString() === $currentDate->toDateString())
-                                <a href="{{ route('registrations.show', $opportunity->oppId) }}" class="inline-block px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 transition duration-300">
-                                    View Volunteer List
-                                </a>
-                            @else
-                                <button onclick="showAlert()" class="inline-block px-4 py-2 text-white bg-gray-400 rounded cursor-not-allowed">
-                                    View Volunteer List
-                                </button>
-                            @endif
-                        </td>
                     </tr>
                 @endforeach
                 </tbody>
@@ -42,4 +33,3 @@
         @endif
     </div>
 @endsection
-
